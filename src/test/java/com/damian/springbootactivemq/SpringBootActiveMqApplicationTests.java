e1cb5366a417368319c8b8cb7b863d1e80acb616
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-06-06 17:43:00 +0530

    Initial Commit.

---
+++ src/test/java/com/damian/springbootactivemq/SpringBootActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.springbootactivemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActiveMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
